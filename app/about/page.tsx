9f9f28401075bd8eb978f30aea2837b475d0e821
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-07-26 15:26:05 +0530

    completed stage-1

---
+++ app/about/page.tsx
@@ -0,0 +1,64 @@
+export default function About() {
+  return (
+    <div className="space-y-12 text-md md:text-lg text-muted-foreground pb-4 md:pb-8">
+      <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
+        🖼️ Introducing Morph!! : Your Free Multimedia Transformer
+      </h2>
+      <p>
+        Morph is your go-to tool for effortlessly converting images, audio, and
+        video files. Completely free and packed with powerful features, Morph
+        unleashes your creativity without limits.
+      </p>
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
+          🖼️ File Conversion:
+        </h2>
+        <p>
+           Resize, crop, rotate, and convert image
+          formats with ease. From JPEGs to PNGs, we've got you covered. Audio:
+          Convert between popular audio formats like MP3, WAV, and AAC. Adjust
+          bitrates, trim, and merge your audio files. Video: Edit and convert
+          your videos hassle-free. Change formats, cut, and combine clips to
+          create stunning visuals.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
+        Why Choose Morph?
+        </h2>
+        <p>
+           Completely Free: Enjoy unlimited conversions without
+          spending a dime. Easy to Use: Our intuitive interface makes file
+          conversion simple for everyone. Secure: Your files are safe with us.
+          We prioritize your privacy and data security. Always Improving: We're
+          constantly adding new features to enhance your experience.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
+          🎥 Video Metamorphosis:
+        </h2>
+        <p>
+          Lights, camera, action! Edit and transcode videos without limits. From
+          changing video formats to cutting and merging clips, you can create
+          stunning video content for any platform or purpose.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
+          🚀 Unlimited Usage, No Strings Attached:
+        </h2>
+        <p>
+          Unlike other services, we believe in empowering your creativity
+          without hidden fees or restrictions. You can convert as many images,
+          audio files, and videos as your heart desires, and it won't cost you a
+          dime.
+        </p>
+      </div>
+
+    </div>
+  );
+}
